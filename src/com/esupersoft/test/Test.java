a5c093e20d5fd0779aae90ee97e54589dfd1cb81
Kailash Karigar <karigarkc@gmail.com>
2012-12-11 21:54:23 -0800

    Intializing eclipse project with sample java file.

---
+++ src/com/esupersoft/test/Test.java
@@ -0,0 +1,12 @@
+package com.esupersoft.test;
+
+public class Test {
+
+	/**
+	 * @param args Command line arguments.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
